89a00dd94d9f14e33adbddae90bcc7e045a159a3
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-09-11 00:24:16 +0500

    mod: added author

--- js/main.js
+++ js/main.js
@@ -1,8 +1,56 @@
 $(document).ready(function () {
 	$('.navbar-burger').on('click', function () {
-		console.log('Клик по кнопке');
 		$('.navbar-burger, .menu__list').toggleClass('active')
 		$('body').toggleClass('lock')
 	});
-  AOS.init();
+	AOS.init();
+	// Плавная прокрутка в меню
+
+	let anchors = document.querySelectorAll('a[href*="#"]');
+
+	for (anchor of anchors) {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault()
+			anchorId = this.getAttribute('href')
+			document.querySelector(anchorId).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			})
+		})
+	}
+	// Модальные окна и несколько доп.функций
+
+	const triggerBtns = document.querySelectorAll('[data-btn="offer"]'),
+		closeModalBtn = document.querySelector('[data-close="modal"]'),
+		modal = document.querySelector('.modal');
+
+
+	function openModal() {
+		modal.classList.add('show', 'fade');
+		modal.classList.remove('hide');
+		document.body.style.overflow = 'hidden';
+	}
+	triggerBtns.forEach(btn => {
+		btn.addEventListener('click', openModal)
+	})
+
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show', 'fade');
+		document.body.style.overflow = '';
+	}
+
+	closeModalBtn.addEventListener('click', closeModal)
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
+		}
+	})
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code == 'Escape') {
+			closeModal();
+		}
+	})
 })
